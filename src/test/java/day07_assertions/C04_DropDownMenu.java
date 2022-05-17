14a14b0cfaf7a7284316d0c87d8647049bc11f3d
azizsiler <azizsiler@gmail.com>
2022-05-17 21:53:04 +0300

    17.05.2022

--- src/test/java/day07_assertions/C04_DropDownMenu.java
+++ src/test/java/day07_assertions/C04_DropDownMenu.java
@@ -1,7 +1,7 @@
 package day07_assertions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,52 +10,45 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.time.Duration;
-
 public class C04_DropDownMenu {
-
-   /* amazon'a gidip
-   dropdown'dan books secenegini secip
-   Java aratalim
-   ve arama sonuclarinin Java icerdigini test edelim
- */
-WebDriver driver;
-
-
-@Before
+    /* amazon'a gidip
+       dropdown'dan books secenegini secip
+       Java aratalim
+       ve arama sonuclarinin Java icerdigini test edelim
+     */
+    WebDriver driver;
+    @Before
     public void setUp(){
-    WebDriverManager.chromedriver().setup();
-    driver=new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-}
-@After
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
     public void tearDown(){
-    driver.quit();
-}
-
-@Test
-    public void test01() throws InterruptedException {
-    driver.get("https://www.amazon.com/");
-
-    WebElement dropDownMenu=driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
-    Select select=new Select(dropDownMenu);
-    select.deselectByVisibleText("Books"); // gorunur isimle seciyor
-    //select.deselectByIndex(); // index ile sesiyor
-    //select.selectByValue();
-
-
-    // Arama kutusuna java yazdiralim
-
-    WebElement aramaKutusu=driver.findElement(By.id("twotasearchtextbox"));
-    aramaKutusu.sendKeys("Java"+ Keys.ENTER);
-
-    Thread.sleep(5000);
-}
-
-
-
-
+        driver.quit();
+    }
+    @Test
+    public void test02() throws InterruptedException {
+        driver.get("https://www.amazon.com");
+        // dropdown'dan bir option secmek icin 3 adim vardir
+        // 1- dropdown'i locate edelim
+        WebElement dropDownMenu= driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+        // 2- bir Select objesi olusturup
+        //    parametre olarak bir onceki adimda locate ettigimiz ddm'yu girelim
+        Select select=new Select(dropDownMenu);
+        // 3- Dropdown'da var olan option'lardan istedigimiz bir taneyi secelim
+        select.selectByVisibleText("Books");
+        // select.selectByIndex(5);
+        // select.selectByValue("search-alias=stripbooks-intl-ship");
+        // arama kutusuna Java yazdiralim
+        WebElement aramaKutusu= driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Java" + Keys.ENTER);
+        WebElement sonucYazisiElementi=driver.findElement(By.xpath("(//div[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        String sonucYazisiStr=sonucYazisiElementi.getText();
+        String arananKelime="Java";
+        Assert.assertTrue(sonucYazisiStr.contains(arananKelime));
+        Thread.sleep(5000);
+    }
 }
