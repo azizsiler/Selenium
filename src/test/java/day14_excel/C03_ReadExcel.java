8ea5a1258fc8186d632a8c231e4d6cfff95137d3
azizsiler <azizsiler@gmail.com>
2022-05-24 22:49:34 +0300

    240522

--- src/test/java/day14_excel/C03_ReadExcel.java
+++ src/test/java/day14_excel/C03_ReadExcel.java
@@ -1,2 +1,46 @@
-package day14_excel;public class C03_ReadExcel {
+package day14_excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C03_ReadExcel {
+    @Test
+    public void readExcelTest() throws IOException {
+        // input olarak verilen
+        // satirNo sutunNo degerlerini parametre olarak alip
+        // o cell'deki datayai string olarak bana donduren bir method olusturu
+
+        int satirNo=4;
+        int sutunNo=4;
+
+
+        // donen stringin Cezair oldugunu test edin
+
+        String expectedData="Cezayir";
+
+      String actualData=  banaDataGetir(satirNo-1,sutunNo-1); // cunku excel index ile calisir
+
+        Assert.assertEquals(expectedData,actualData);
+    }
+
+    public static String  banaDataGetir(int satirIndex, int sutunIndex) throws IOException {
+
+
+        String istenenData="";
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+        Workbook workbook= WorkbookFactory.create(fis);
+        istenenData=workbook
+                .getSheet("Sayfa1").
+                getRow(satirIndex).
+                getCell(sutunIndex).
+                toString();
+        return istenenData;
+    }
 }
